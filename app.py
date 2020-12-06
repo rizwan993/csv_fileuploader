15fa5b01a538cf3a0a602e0804bc311eb9103401
rizwan993 <rizwanzakariya09@gmail.com>
2020-12-07 00:03:28 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask , render_template, request, redirect
+import pandas as pd
+import csv
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET', 'POST'])
+def index():
+        if request.method == 'POST':
+                csv_file = request.form['csvinput']
+                data = []
+                with open(csv_file) as f :
+                        csv_reader = csv.reader(f)
+                        for row in csv_reader:
+                            data.append(row)
+                return render_template('data.html', data=data)
+        else:
+                return  render_template('index.html')
+
+
+
+
+        
+                
+
+
+
+if __name__ == '__main__' :
+    app.run(debug= True)
+
